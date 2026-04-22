73b240b103536338507214ce9b1897eff4a4a6e3
Liam R Zadoorian <41171966+LiamZadoorian@users.noreply.github.com>
2026-04-21 22:34:40 -0500

    Good Batch

---
+++ Ch15_Pr110_SmartHomeApplianceControl/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CSCI_3000_CH15
+//
+//  Created by Liam Riley Zadoorian on 4/21/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
