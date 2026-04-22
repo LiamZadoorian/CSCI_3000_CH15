4ef01d3f05ac813110d0b14d0ae245a9f897e319
Liam R Zadoorian <41171966+LiamZadoorian@users.noreply.github.com>
2026-04-21 23:13:24 -0500

    Great Batch

--- Ch15_Pr112_RideBookingApplication/main.cpp
+++ Ch15_Pr112_RideBookingApplication/main.cpp
@@ -1,14 +1,38 @@
-//
-//  main.cpp
-//  CSCI_3000_CH15
-//
-//  Created by Liam Riley Zadoorian on 4/21/26.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
+
+class Ride {
+    public:
+        virtual void bookRide() = 0;
+};
+
+class Bike : public Ride {
+    public:
+        void bookRide(){
+            cout << "Bike ride booked successfully\n";
+        }
+};
+
+class Car : public Ride {
+    public:
+        void bookRide(){
+            cout << "Car ride booked successfully\n";
+        }
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    Ride *rd_pntr;
+    
+    Bike bike;
+    Car car;
+    
+    rd_pntr = &bike;
+    rd_pntr->bookRide();
+    
+    rd_pntr = &car;
+    rd_pntr->bookRide();
+    
     return 0;
 }
