a03df18248b0c2e78220bbc1ffcd99535001a623
Liam R Zadoorian <41171966+LiamZadoorian@users.noreply.github.com>
2026-04-21 22:45:57 -0500

    Great Batch
    Implemented a notification system with Email and SMS classes.

--- Ch15_Pr108_SmartDeliveryNotificationSystem/main.cpp
+++ Ch15_Pr108_SmartDeliveryNotificationSystem/main.cpp
@@ -1,14 +1,40 @@
-//
-//  main.cpp
-//  CSCI_3000_CH15
-//
-//  Created by Liam Riley Zadoorian on 4/21/26.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
+
+class Notification{
+    public:
+        virtual void sendMessage(){
+            cout << "Sending General Notification\n";
+        }
+};
+
+class Email : public Notification {
+    public:
+        void sendMessage(){
+            cout << "Sending Email Notification\n";
+        }
+};
+
+class SMS : public Notification {
+    public:
+        void sendMessage(){
+            cout << "Sending SMS Notification\n";
+        }
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    Notification *notif_pntr;
+    
+    Email email;
+    SMS sms;
+    
+    notif_pntr = &email;
+    notif_pntr -> sendMessage();
+    
+    notif_pntr = &sms;
+    notif_pntr -> sendMessage();
+    
     return 0;
 }
