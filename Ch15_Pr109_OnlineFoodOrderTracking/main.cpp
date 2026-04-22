c0e501688640d9fe0b576de35b05c10289799b70
Liam R Zadoorian <41171966+LiamZadoorian@users.noreply.github.com>
2026-04-21 22:54:07 -0500

    Great Batch

--- Ch15_Pr109_OnlineFoodOrderTracking/main.cpp
+++ Ch15_Pr109_OnlineFoodOrderTracking/main.cpp
@@ -1,14 +1,52 @@
-//
-//  main.cpp
-//  CSCI_3000_CH15
-//
-//  Created by Liam Riley Zadoorian on 4/21/26.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
+
+class OrderStatus{
+    public:
+        virtual void showStatus(){
+            cout << "Order is undecided\n";
+        }
+    
+};
+
+class Preparing : public OrderStatus{
+    public:
+        void showStatus(){
+            cout << "Order is being prepared\n";
+        }
+};
+
+class OutForDelivery : public OrderStatus{
+    public:
+        void showStatus(){
+            cout << "Order is out for delivery\n";
+        }
+};
+
+class Delivered : public OrderStatus{
+    public:
+        void showStatus(){
+            cout << "Order has been delivered\n";
+        }
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    OrderStatus *stat_pntr;
+    
+    Preparing prep;
+    OutForDelivery ofd;
+    Delivered dlvrd;
+    
+    stat_pntr = &prep;
+    stat_pntr->showStatus();
+    
+    stat_pntr = &ofd;
+    stat_pntr->showStatus();
+    
+    stat_pntr = &dlvrd;
+    stat_pntr->showStatus();
+    
     return 0;
 }
